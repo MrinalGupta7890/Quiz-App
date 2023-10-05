c58e1a9c7eb2d1c1591feda58bfe219cbd098b84
Mrunal Gupta <125861423+MrinalGupta7890@users.noreply.github.com>
2023-10-05 22:11:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+const questions = [
+    {
+        question: "What is the largest animal on the world?",
+        answeres:[
+            {text:"Elephant", correctAnswer : false},
+            {text:"Shark", correctAnswer : false},
+            {text:"Blue Whale", correctAnswer : true},
+            {text:"Giraffe", correctAnswer : false},
+        ]
+    },
+
+    {
+        question: "Which is the smallest country in the world?",
+        answeres:[
+            {text:"Vatican city", correctAnswer : true},
+            {text:"Bhutan", correctAnswer : false},
+            {text:"Nepal", correctAnswer : false},
+            {text:"SriLanka", correctAnswer : false},
+        ]
+    },
+
+    {
+        question: "What is the largest animal on the world?",
+        answeres:[
+            {text:"Elephant", correctAnswer : false},
+            {text:"Shark", correctAnswer : false},
+            {text:"Blue Whale", correctAnswer : true},
+            {text:"Giraffe", correctAnswer : false},
+        ]
+    },
+
+    {
+        question: "What is the largest animal on the world?",
+        answeres:[
+            {text:"Elephant", correctAnswer : false},
+            {text:"Shark", correctAnswer : false},
+            {text:"Blue Whale", correctAnswer : true},
+            {text:"Giraffe", correctAnswer : false},
+        ]
+    },
+
+    {
+        question: "What is the largest animal on the world?",
+        answeres:[
+            {text:"Elephant", correctAnswer : false},
+            {text:"Shark", correctAnswer : false},
+            {text:"Blue Whale", correctAnswer : true},
+            {text:"Giraffe", correctAnswer : false},
+        ]
+    },
+]
